d638e958bf2ff92cfcde21d017c3f5cd17d2ef38
Moulberry <james.jenour@protonmail.com>
2023-08-28 03:18:34 +0800

    Add forEach API to BooleanRegion

--- src/main/java/com/moulberry/axiomclientapi/regions/BooleanRegion.java
+++ src/main/java/com/moulberry/axiomclientapi/regions/BooleanRegion.java
@@ -1,6 +1,7 @@
 package com.moulberry.axiomclientapi.regions;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.moulberry.axiomclientapi.funcinterfaces.TriIntConsumer;
 import net.minecraft.client.Camera;
 import net.minecraft.world.phys.Vec3;
 import org.joml.Matrix4f;
@@ -8,13 +9,12 @@ import org.joml.Matrix4f;
 public interface BooleanRegion {
 
     void render(Camera camera, Vec3 translation, PoseStack matrix, Matrix4f projection, long time, int effects);
+    void forEach(TriIntConsumer consumer);
 
     boolean add(int x, int y, int z);
 
     void close();
     void clear();
     boolean contains(int x, int y, int z);
-
-
 
 }
